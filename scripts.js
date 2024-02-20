cdb50656e852c57db047cec4f6c9f3ec3fca3c23
feliperyo <ryofeliperyo@gmail.com>
2024-02-20 08:05:53 -0300

    adicionando Regra de Negócio

--- scripts.js
+++ scripts.js
@@ -8,16 +8,15 @@ function sorteiaNumero() {
     const min = Math.ceil(document.querySelector("#min").value)
     const max = Math.floor(document.querySelector("#max").value)
 
+    if (min >= max) {
+        alert("O valor mínimo tem que ser MENOR que o valor máximo")
+    } else {
+        const result = Math.floor(Math.random() * (max - min + 1)) + min;
 
-    const result = Math.floor(Math.random() * (max - min + 1)) + min;
-
-    numSorteado.innerHTML = result
-    numSorteado.style.visibility = "visible"
-    numSorteado.style.fontSize = "50px"
-
-
-    console.log(min, max)
-    console.log(result)
+        numSorteado.innerHTML = result
+        numSorteado.style.visibility = "visible"
+        numSorteado.style.fontSize = "50px"
+    }
 }
 
 clickButton.addEventListener("click", sorteiaNumero)
